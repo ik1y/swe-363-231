73c408eda649c0a0e24b41349b565b2af9562e72
ik1y <7amod.333@gmail.com>
2023-10-27 21:23:33 +0300

    add js4 ex 3

--- Task1/script.js
+++ Task1/script.js
@@ -35,10 +35,10 @@ for(let post of posts){
     document.querySelector("#Games").appendChild(section)
 }*/
 class CreateSection {
-    constructor(src, title,brief) {
+    constructor(src, title, brief) {
         this.title = title;
         this.brief = brief;
-        this.src=src;
+        this.src = src;
     }
 }
 let posts =
@@ -51,7 +51,7 @@ let posts =
             "Dark souls 3",
             "Dark Souls 3 takes place in a transitory place, revolving around the first flame and the cycle of fire and dark. You play as a protagonist whose goal is to find and return the five Lords of Cinder to their thrones at Firelink Shrine in order to link the flame again.")
     ]
-function fillContent(posts){
+/*function fillContent(posts){
     for(let post of posts){
         let section=document.createElement("section")
         let p = document.createElement("p")
@@ -68,6 +68,24 @@ function fillContent(posts){
         section.append(img)
         document.querySelector("#Games").appendChild(section)
     }
+}*/
+function fillContent(posts) {
+    const addElements = posts.map((post) => {
+        let section = document.createElement("section")
+        let p = document.createElement("p")
+        let h2 = document.createElement("h2")
+        let img = document.createElement("img")
+        img.src = post.src
+        img.alt = "Game Image"
+        img.style.height = '300px'
+        img.style.width = '400px'
+        p.innerHTML = post.brief
+        h2.innerHTML = post.title
+        section.append(h2)
+        section.append(p)
+        section.append(img)
+        document.querySelector("#Games").appendChild(section)
+    });
 }
 fillContent(posts);
 
